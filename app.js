e34760c20e3f33225f8d95331e1825d55db7e54e
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-08-22 21:07:30 +0600

    login page logo added and login button functionality added

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 document.getElementById('login-btn')
 .addEventListener('click',function(e){
     e.preventDefault()
+    const mobileNumber = document.getElementById('mobile-number').value
+    
+
+    const pinNumber = document.getElementById('pin-number').value
+    
+
+    if(mobileNumber.length === 11 && !isNaN(mobileNumber) && pinNumber.length === 4 && !isNaN(pinNumber)){
+       window.location.href="./home.html"
+    }
+
+    else{
+        alert('invalid credentials')
+    }
+
 })
